ddade5bbd8cf97539df0978d783fb405dce355b4
suprabha <suprabha.suprabha@go-jek.com>
2020-05-16 22:47:21 +0530

    feat(DB): added db connection

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
 )
 
+// These are the three models atruct which has been created
 type User struct {
 	ID       int    `json: "id:`
 	Email    string `json: "email"`
@@ -22,7 +25,27 @@ type Error struct {
 	msg string
 }
 
+// creating new variable db
+var db *sql.DB
+
 func main() {
+	pgUrl, err := pq.ParseURL("POSTGRESURL")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// invoke the open sql
+	// first parameter is driver name
+	db, err = sql.Open("postgres", pgUrl)
+	// fmt.Println("db--->", db) // we will get all details of DB
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ping will return once value, if there is a connection established successfully the response,will return nil
+	err = db.Ping()
+
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", signup).Methods("POST")
 	router.HandleFunc("/login", login).Methods("POST")
